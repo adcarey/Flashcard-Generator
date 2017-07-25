38ad2828a16c8cc19344788592d5837a986fda84
adcarey <ad.carey1990@gmail.com>
2017-07-24 21:59:17 -0600

    fist updates

---
+++ ClozeCard.js
@@ -0,0 +1,9 @@
+
+
+exports.ClozeCard = function(full, cloze){
+	this.full = full;
+	this.cloze = cloze;
+	this.partial = full.replace(cloze, "....");
+}; 
+
+
---
+++ questions.js
@@ -0,0 +1,14 @@
+exports.questions =[
+{
+	full: "9 is the square root of 81",
+	cloze: "9"
+},
+{
+	full: "There are 365 days in a year",
+	cloze: "365"
+},
+{
+	full: "There are 7 Harry Potter Books",
+	cloze: "7"
+}
+]
